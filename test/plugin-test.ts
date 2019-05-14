dd9db3b7913f5b09bc18bad1f3da4b53e1676d22
Hiroyuki Anai <pirosikick@gmail.com>
2019-05-14 15:27:20 +0900

    fix getAssetPaths unit test for windows

--- test/plugin-test.ts
+++ test/plugin-test.ts
@@ -2,19 +2,19 @@ import * as assert from "assert";
 import * as path from "path";
 import * as plugin from "../src/plugin";
 
-const SAMPLE_PLUGIN_PATH = path.join(__dirname, "fixtures/sample/plugin");
-const MANIFEST_JSON_FILE = path.join(SAMPLE_PLUGIN_PATH, "manifest.json");
+const SAMPLE_PLUGIN_PATH = path.resolve(__dirname, "fixtures/sample/plugin");
+const MANIFEST_JSON_FILE = path.resolve(SAMPLE_PLUGIN_PATH, "manifest.json");
 
 describe("plugin", () => {
   describe("getAssetPaths", () => {
     it("should return asset file paths from the manifest.json", () => {
       assert.deepStrictEqual(plugin.getAssetPaths(MANIFEST_JSON_FILE), [
-        `${SAMPLE_PLUGIN_PATH}/js/customize.js`,
-        `${SAMPLE_PLUGIN_PATH}/css/desktop.css`,
-        `${SAMPLE_PLUGIN_PATH}/js/customize.js`,
-        `${SAMPLE_PLUGIN_PATH}/css/mobile.css`,
+        path.resolve(SAMPLE_PLUGIN_PATH, 'js/customize.js'),
+        path.resolve(SAMPLE_PLUGIN_PATH, 'css/desktop.css'),
+        path.resolve(SAMPLE_PLUGIN_PATH, 'js/customize.js'),
+        path.resolve(SAMPLE_PLUGIN_PATH, 'css/mobile.css'),
         MANIFEST_JSON_FILE,
-        `${SAMPLE_PLUGIN_PATH}/image/icon.png`
+        path.resolve(SAMPLE_PLUGIN_PATH, 'image/icon.png')
       ]);
     });
   });
